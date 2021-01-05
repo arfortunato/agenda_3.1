2256330cefb0e1ca1b94ec8ab336da49d7796d95
Artur <Artur>
2021-01-05 10:14:49 -0300

    terminando modulo 3.1

---
+++ Agenda/Mapa/Controller/MapaViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MapaViewController.swift
+//  Agenda
+//
+//  Created by Artur Rodrigues Fortunato on 04/01/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapaViewController: UIViewController {
+    
+    //MARK - outlet
+    @IBOutlet weak var mapa: MKMapView!
+    
+    //MARK: - Variavel
+    
+    var aluno:Aluno?
+    
+    //MARK: - View life Cyrcle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = getTItulo()
+        localizacaoInicial()
+        localizarAluno()
+
+        
+    }
+    
+    //MARK: - Metodos
+    
+    func getTItulo() -> String{
+        return "Localizar Alunos"
+    }
+    
+    func localizacaoInicial(){
+        Localizacao().converteEnderecoEmCoordenadas(endereco: "Caelum - São Paulo") { (localizacaoEncontrada) in
+            let pino = self.configuraPino(titulo: "Caelum", localizacao: localizacaoEncontrada)
+            let regiao = MKCoordinateRegionMakeWithDistance(pino.coordinate, 5000, 5000)
+            
+            self.mapa.setRegion(regiao, animated: true)
+            self.mapa.addAnnotation(pino)
+        }
+    }
+    
+    func localizarAluno(){
+        if let aluno = aluno{
+            Localizacao().converteEnderecoEmCoordenadas(endereco: aluno.endereco!) { (localizacaoEncontrada) in
+                let pino = self.configuraPino(titulo: aluno.nome!, localizacao: localizacaoEncontrada)
+                self.mapa.addAnnotation(pino)
+            }
+        }
+        
+    }
+    
+    func configuraPino(titulo:String, localizacao:CLPlacemark) ->MKPointAnnotation{
+        let pino = MKPointAnnotation()
+        
+        pino.title = titulo
+        pino.coordinate = localizacao.location!.coordinate
+        
+        
+        return pino
+    }
+
+}
